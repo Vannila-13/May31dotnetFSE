d413dfa5085f95e940a8d700a519e72403fe9462
Vannila <vannilas13@yahoo.com>
2021-05-31 18:17:45 +0800

    Day1 EOD

---
+++ Day1/FirstVSApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FirstVSApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            num1 = 50;
+            num2 = 100;
+            sum = num1 + num2;
+            Console.WriteLine($"The Sum is {sum}");
+            Console.WriteLine("Learn Selection");//if statement
+            if(num1==num2)
+             Console.WriteLine("The numbers are equal");
+
+                else if (num1 > num2)
+                    Console.WriteLine($"{num1}  which is num1 is greater");
+                else
+                    Console.WriteLine($"{num2} which is num2 is greater");
+            Console.WriteLine("Learn Iteration");//for loop
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(i);
+             }
+            int count = 0;//while statement
+            while(count<100)
+            {
+                Console.WriteLine(count);
+                count = count + 10;
+            }
+            int n = 0;
+            do
+            {
+                Console.WriteLine("tick" + n);
+                n++;
+            } while (n < 10);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day1/LeftRotate1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeftRotate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nd = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(nd[0]);
+            int d = Convert.ToInt32(nd[1]);
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+
+            d = d % n;
+            for(int i=d;i<a.Length;i++)
+            {
+                Console.Write($"{a[i]}");
+            }
+            for(int i=0;i<d;i++)
+            {
+                Console.Write($"{a[i]}");
+            }
+        }
+    }
+}
+
